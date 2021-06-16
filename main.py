05f4e83e9039f7cde77e7548442a6ba36384ea5e
Stanslaw <33247204+Stanslaw@users.noreply.github.com>
2021-06-16 17:28:19 +0300

    View fear graph

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 api_fear = 'https://api.alternative.me/fng/?limit=0&date_format=us'
 api_btc_cost = 'https://api.alternative.me/v2/ticker/bitcoin/?limit=100'
@@ -15,14 +16,15 @@ data_json = json.loads(data_fear.text)
 data_fear_panda = pd.DataFrame(data_json['data'])
 
 #Делаем дату датой
-data_fear_panda.timestamp = list(map(lambda x: datetime.strptime(x, '%m-%d-%Y'), data_fear_panda.timestamp))
+data_fear_panda.timestamp = pd.to_datetime(data_fear_panda.timestamp)
 
-data_fear_panda.reindex(index=data_fear_panda.timestamp)
+#Делаем Числа числами
+data_fear_panda.value = pd.to_numeric(data_fear_panda.value)
 
-print(data_fear_panda)
+graph = data_fear_panda[['timestamp', 'value', 'value_classification']]
 
-# graph = data_fear_panda[['timestamp', 'value']]
-#
-# graph.plot()
+print(graph)
 
-# plt.show()
+ax1 = graph.plot(x='timestamp', y='value', grid=True)
+
+plt.show()
